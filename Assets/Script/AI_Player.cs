bd890e99edc2135ea90a5279fbd66d6bfe67e923
Matthias <117727012+techassurly@users.noreply.github.com>
2023-07-10 11:25:22 +0200

    Add, Top down camera

---
+++ Assets/Script/AI_Player.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Player : MonoBehaviour
+{
+
+    public enum ColorState { BLUE, YELLOW, RED, GREEN };
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
