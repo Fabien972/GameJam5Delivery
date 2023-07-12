fbaf36337448e9ab1c14afdbce382e9aaa52364d
Rodrigue <74072465+DumezRodrigue@users.noreply.github.com>
2023-07-12 15:30:45 +0200

    Fix MailBoxes

--- Assets/Scripts/MailBox.cs
+++ Assets/Scripts/MailBox.cs
@@ -4,14 +4,5 @@ using UnityEngine;
 
 public class MailBox : MonoBehaviour
 {
-
-    private void OnTriggerEnter(Collider other)
-    {
-        AI_Player ai = other.GetComponent<AI_Player>();
-        if (ai != null)
-        {
-            ai.deliveries.Remove(this);
-            ai.NextDelivery();
-        }
-    }
+    public bool isDelivered = false;
 }
